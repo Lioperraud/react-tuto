2183e07e998046fe32ceebb32ad961b45c3354ca
Lioperraud <lioperraud@gmail.com>
2026-01-30 14:42:23 +0100

    Ajout fonctionnalité chrono

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState, useEffect } from "react";
 import './App.css'
 import TodoList from "./components/TodoList";
+import Chrono from "./components/Chrono";
 
 function App() {
 
   return (
-    <div>
+    <div className="app">
        <TodoList />
+       <Chrono />
     </div>
   );
 }
